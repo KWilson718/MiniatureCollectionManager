e1d2e20edc0905ca945c21c9fd59e5a7544b0b9b
KWilson718 <wilsonk18@spu.edu>
2024-08-21 20:08:37 -0700

    Header is now set up stylewise

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
   import Header from './components/Header.vue';
+
+
 </script>
 
 <template>
   <v-app style="width: 100%;">
     <v-layout class="rounded rounded-md"  style="width: 100%;">
-      <v-app-bar title="Miniature Collection Manager">
-        <Header></Header>
-      </v-app-bar>
+      <Header></Header>
       <v-main class="d-flex align-center justify-center">
         <router-view />
       </v-main>
