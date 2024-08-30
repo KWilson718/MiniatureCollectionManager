41d1083439044be41e8f65fe90e8b11bbdbf15db
KWilson718 <wilsonk18@spu.edu>
2024-08-30 00:59:50 -0700

    I believe data writes to DB

--- src/composables/formAssistants.ts
+++ src/composables/formAssistants.ts
@@ -1,14 +1,32 @@
 // import { ref } from 'vue';
 import { generateID } from "../modules/generateID";
+import { Category, insertCategory } from "../lib/dbController";
 
 export function createCatSubmit() {
     const submit = async (data: any) => {
         try {
             // Generate the ID and wait for the result
             const randomID = await generateID();
-            console.log('Form submitted with data:', data);
-            console.log('ID Generated:', randomID);
-            // Add your submission logic here (e.g., API calls)
+
+            const now = new Date();
+
+            const metadata = {
+                createdAt: now.toLocaleString(),
+                updatedAt: now.toLocaleString(),
+            }
+
+            let payload: Category = {
+                id: randomID,
+                name: data.categoryName,
+                description: data?.categoryDesc,
+                metadata: metadata,
+                subcategories: [],
+            }
+
+            let insertion = await insertCategory(payload);
+
+            console.log(insertion);
+
         } catch (error) {
             console.error('Error generating ID or submitting form:', error);
         }
