32b1521da3b16e857146fa60ec26f722d1ea4173
KWilson718 <wilsonk18@spu.edu>
2025-01-01 22:34:31 -0800

    filled in a bit more of the data structure

--- components/filterList/testData.js
+++ components/filterList/testData.js
@@ -1,5 +1,42 @@
 const data = [
-    { id: 1, title: 'Games Workshop'},
+    { 
+        id: 1, 
+        title: 'Games Workshop',
+        games: [
+            {
+                title: 'Warhammer 40k',
+                factions: [
+                    {
+                        factionID: 1,
+                        title: 'Space Marines',
+                        description: 'Genetically Modified Super Soldiers of the Imperium'
+                    },
+                    {
+                        factionID: 2,
+                        title: 'Orks',
+                        description: 'A Mass Horde of Space Orks which grow as a fungal infestation of a world'
+                    }
+                ]
+            },
+            {
+                title: 'Warhammer Age of Sigmar',
+                factions: [
+                    {
+                        factionID: 3,
+                        title: 'Orruk Warclans',
+                    },
+                    {
+                        factionID: 4,
+                        title: 'Stormcast Eternals'
+                    },
+                    {
+                        factionID: 5,
+                        title: 'Gloomspite Gitz'
+                    }
+                ]
+            }
+        ]
+    },
     { id: 2, title: 'Atomic Mass Games'},
     { id: 3, title: 'Warlord Games'},
     { id: 4, title: 'Wargames Atlantic'},
